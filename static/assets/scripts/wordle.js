f047c899d89e5aff9f1d575c4a4ee84f7ae0a531
RattatAndyGo <andries66deklerck@gmail.com>
2023-01-24 23:03:35 +0100

    added basics to wordle backend

--- static/assets/scripts/wordle.js
+++ static/assets/scripts/wordle.js
@@ -1,26 +1,32 @@
 let rows = 6;               // Amount of displayed rows
 let columns = 5;            // Amount of letters in a word
-let guessedWords = [];      // Array of guessed words
+let currentGuess = "";      // Current guess, possibly not fully typed yet
 let answer = "blank";       // Answer of the puzzle
+let guessedWords = ["tired", "added"];      // Array of guessed words
 
 window.onload = function(){
     draw();
-
-    document.addEventListener("keydown", processKeystroke());
 }
+
+// Keyboard listener to detect keystrokes
+document.addEventListener("keyup", (e) => {processKeystroke(e.key)});
 
 // draws the HTML on screen
 function draw(){
     // game board
     let field_html = '';
-    for(let i = 0; i < rows; i++){
+    for(let i = 0; i < guessedWords.length; i++){
         let row_html = "<tr>";
-        for(let j = 0; j < guessedWords.length; j++){
-            row_html += `<td>${guessedWords[i][j]}</td>`;
+        for(let j = 0; j < columns; j++){
+            row_html += `<td id="cell${i}-${j}"><p>${guessedWords[i][j].toUpperCase()}</p></td>`;
         }
-        // current typed guess
-        for(let j = guessedWords.length; j < columns; j++){       // if less than 5 guesses, fill with empty guesses
-            row_html += "<td></td>";
+        row_html += "</tr>";
+        field_html += row_html;
+    }
+    for(let i = guessedWords.length; i < rows; i++){       // if less than 5 guesses, fill with empty guesses
+        let row_html = '<tr>';
+        for(let j = 0; j < columns; j++){
+            row_html += `<td id="cell${i}-${j}"></td>`
         }
         row_html += "</tr>";
         field_html += row_html;
@@ -38,6 +44,17 @@ function get_row_html(index){
 }
 
 // Runs every time a key is pressed, and does something depending on the key in question
-function processKeystroke(){
+function processKeystroke(key){
+    key = String(key);
+    if(key == "Enter") guess();
+    if(key == "Backspace") currentGuess = currentGuess.slice(0, currentGuess.length - 2);
+    if(key != RegExp){
+        return;
+    }
+    // code
+}
+
+// Submit a word to check against the answer
+function guess(){
 
 }
