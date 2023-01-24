ec321909ece8e7038174b2fd34835722fff4cdc8
RattatAndyGo <andries66deklerck@gmail.com>
2023-01-25 00:24:25 +0100

    frontend playable (needs keyboard & rules), started backend logic

--- static/assets/scripts/wordle.js
+++ static/assets/scripts/wordle.js
@@ -1,30 +1,44 @@
-let rows = 6;               // Amount of displayed rows
+let rows = 6;               // Minimum amount of displayed rows
 let columns = 5;            // Amount of letters in a word
 let currentGuess = "";      // Current guess, possibly not fully typed yet
 let answer = "blank";       // Answer of the puzzle
-let guessedWords = ["tired", "added"];      // Array of guessed words
+let guessedWords = [];      // Array of guessed words
 
 window.onload = function(){
     draw();
 }
 
 // Keyboard listener to detect keystrokes
-document.addEventListener("keyup", (e) => {processKeystroke(e.key)});
+document.addEventListener("keydown", (e) => {processKeystroke(e.key)});
 
-// draws the HTML on screen
+// Draws the HTML on screen
 function draw(){
-    // game board
+    
+    // Guessed words
     let field_html = '';
     for(let i = 0; i < guessedWords.length; i++){
-        let row_html = "<tr>";
+        let row_html = `<tr id="row${i}">`;
         for(let j = 0; j < columns; j++){
             row_html += `<td id="cell${i}-${j}"><p>${guessedWords[i][j].toUpperCase()}</p></td>`;
         }
         row_html += "</tr>";
         field_html += row_html;
     }
-    for(let i = guessedWords.length; i < rows; i++){       // if less than 5 guesses, fill with empty guesses
-        let row_html = '<tr>';
+
+    // Input row
+    let row_html = `<tr id="input-row">`;
+    for(let i = 0; i < currentGuess.length && i < columns; i++){
+        row_html += `<td id="input${i}">${currentGuess[i]}</td>`
+    }
+    for(let i = currentGuess.length; i < columns; i++){
+        row_html += `<td id="input${i}"></td>`;
+    }
+    row_html += "</tr>";
+    field_html += row_html;
+
+    // Additional rows if needed
+    for(let i = guessedWords.length; i < rows-1; i++){       // if less than 5 guesses, fill with empty guesses
+        let row_html = `<tr id="row${i}">`;
         for(let j = 0; j < columns; j++){
             row_html += `<td id="cell${i}-${j}"></td>`
         }
@@ -46,15 +60,41 @@ function get_row_html(index){
 // Runs every time a key is pressed, and does something depending on the key in question
 function processKeystroke(key){
     key = String(key);
-    if(key == "Enter") guess();
-    if(key == "Backspace") currentGuess = currentGuess.slice(0, currentGuess.length - 2);
-    if(key != RegExp){
+    if(key == "Enter"){
+        guess();
         return;
     }
-    // code
+    if(key == "Backspace"){
+        currentGuess = currentGuess.slice(0, -1);
+        document.getElementById(`input${currentGuess.length}`).innerHTML = "";
+        return;
+    }
+    if(!key.match(/[a-z]/gi) || currentGuess.length >= columns) return;
+
+    currentGuess += key;
+    document.getElementById(`input${currentGuess.length-1}`).innerHTML = `<p>${key.toUpperCase()}</p>`;
 }
 
 // Submit a word to check against the answer
 function guess(){
-
+    if(currentGuess.length < columns){
+        console.log("not enough letters");
+        return;
+    }
+    guessedWords.push(currentGuess);
+    currentGuess = "";
+    draw();
 }
+
+// TODO
+// guessing algorithm with changing answer
+// adding colors in draw()
+
+// iterate through list, for each word:
+// get the answer colors
+// convert it to an int (see colors as 0 1 2, combo as ternary number)
+// store word in corresponding array (using array of 243 arrays)
+// once through list, select index of array with most elements (if tie use randomizer, prevents optimal strategies somewhat and keeps game fresher)
+// revert to color combo
+// fill in color combo
+// swap list of all possible answers with winning array
