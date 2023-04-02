20357160ac9a478e0c392afcdcfe702eae73858d
RattatAndyGo <andries66deklerck@gmail.com>
2023-04-03 00:18:26 +0200

    bingo generator fully works (up to gen 5), lacks frontend

--- main.py
+++ main.py
@@ -41,28 +41,61 @@ def merge(im1, im2):
     return im
 
 # All Pokemon sprites sourced from https://veekun.com/dex/downloads
-def generateBingoCard(images : list, freeSquare : bool, completed : list, game : str, useShiny = False, width = 5, height = 5):
+def generateBingoCard(images : list, completed : list, game : str, useShiny : bool, width, height):
     im = Image.open("static/assets/images/bingo/bingo_template.jpg").copy()
+    if(useShiny):
+        game = game + "/shiny"
     for i in range(height):
         for j in range(width):
-            pokemon = Image.open("static/assets/images/bingo/{}/{}.png".format(game, images[i*height+j])).resize((279, 279))
-            im.paste(pokemon, (41 + 294*j, 358 + 308*i))
-    im.show()
+            if(images[i*height+j] == -1):
+                pokemon = Image.open("static/assets/images/bingo/free.png")
+            else:
+                pokemon = Image.open("static/assets/images/bingo/{}/{}.png".format(game, images[i*height+j])).resize((279, 279))
+            im.paste(pokemon, (41 + 294*j, 358 + 308*i), pokemon.convert("RGBA"))
+            if(completed[i*height+j]):
+                completed_icon = Image.open("static/assets/images/bingo/completed.png")
+                im.paste(completed_icon, (41 + 294*j, 398 + 308*i), completed_icon.convert("RGBA"))
+    return im
+
+def formatBingoCard(images : list, freeSquare : bool, completed : list, game : str, useShiny = False, width = 5, height = 5):
+    pokemonMaxDict = {"gold":251, "silver":251, "crystal":251, "ruby-sapphire":386, "emerald":386, "firered-leafgreen":386, "diamond-pearl":493, "platinum":493, "heartgold-soulsilver":493, "black-white":649}
+    pokemonMax = pokemonMaxDict[game]
+
+    pokemonAmount = width * height
+    if(freeSquare):
+        pokemonAmount -= 1
+    while(len(images) < pokemonAmount):
+        i = randint(1, pokemonMax)
+        while(i in images):
+            i = randint(1, pokemonMax)
+        images.append(i)
+        completed.append(False)
+    if(freeSquare):
+        images.insert(len(images)//2, -1)
+        completed.insert(len(images)//2, False)
+
+    generateBingoCard(images, completed, game, useShiny, width, height).show()
 
 # TODO
-# scale up images to fixed size
-# figure out exact offsets
-# return image
-# frontend interface + connection to backend
+# alternate forms
+# frontend interface + connection to backend (including all choices)
 
 pokemon = []
 completed = []
 for i in range(25):
-    pokemon.append(randint(1, 493))
-    completed.append(False)
-generateBingoCard(pokemon, 
-                  True, 
+    mon = input("give a pokemon dex number: ")
+    if(mon == ""):
+        break
+    pokemon.append(mon)
+    completed.append(input("give a boolean: ") == "true")
+print(completed)
+freeSquare = (input("should there be a free square in the middle? ") == "yes")
+game = input("what game should the sprites come from? ")
+useShiny = (input("do you want to use shiny sprites? ") == "yes")
+formatBingoCard(pokemon, 
+                  freeSquare, 
                   completed,
-                  "platinum",
+                  game,
+                  useShiny
                  )
 app.run(host="0.0.0.0", port=5000)
