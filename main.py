13fdfc5c4e10feaafd4128474c31590d9ca9ec4c
RattatAndyGo <andries66deklerck@gmail.com>
2023-04-02 12:08:16 +0200

    tested basics for bingo card generating, added all sprites gen 2-5

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import *
 import os
+# import sys
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -27,4 +29,29 @@ def bingoCard():
 def unfairWordle():
     return render_template("unfair-wordle.html")
 
+def merge(im1, im2):
+    w = im1.size[0] + im2.size[0]
+    h = max(im1.size[1], im2.size[1])
+    im = Image.new("RGBA", (w, h))
+
+    im.paste(im1)
+    im.paste(im2, (im1.size[0], 0))
+
+    return im
+
+def generateBingoCard(images, freeSquare : bool,  width = 5, height = 5):
+    im = Image.open("static/assets/images/bingo/bingo_template.jpg").copy()
+    for i in range(width):
+        for j in range(height):
+            pokemon = Image.open("static/assets/images/bingo/rattata.png")
+            im.paste(pokemon, (125 + 300*i, 450 + 300*j))
+    im.show()
+
+# TODO
+# scale up images to fixed size
+# figure out exact offsets
+# return image
+# frontend interface + connection to backend
+
+generateBingoCard([], True)
 app.run(host="0.0.0.0", port=5000)
